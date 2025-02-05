3bcfbfaf0240e90581c518ae768953a55b36f37f
AdityaSohal <adityasohal732@gmail.com>
2025-02-05 23:26:32 +0530

    Solution to Print a Palindromic Number Triangle

--- Print a Palindromic Number Triangle@@67877e1818d53eb724f435d0/code.c
+++ Print a Palindromic Number Triangle@@67877e1818d53eb724f435d0/code.c
@@ -1,26 +1,33 @@
 #include <stdio.h>
 
-int main() {
-    int n;
-    scanf("%d", &n);  // Input the number of rows
-    
-    int num = 1;  // Start number
-
-    // Loop through each row
-    for (int i = 1; i <= n; i++) {
-        // Print leading spaces for pyramid shape
-        for (int j = i; j < n; j++) {
+void print_palindromic_triangle(int N) {
+    for (int i = 1; i <= N; i++) {
+        // Print leading spaces
+        for (int j = i; j < N; j++) {
             printf(" ");
         }
-
-        // Print numbers for the current row
+        
+        // Print increasing part of the number
         for (int j = 1; j <= i; j++) {
-            printf("%d ", num++);
+            printf("%d", j);
         }
-
-        // Move to the next line after each row
+        
+        // Print decreasing part of the number
+        for (int j = i - 1; j >= 1; j--) {
+            printf("%d", j);
+        }
+        
+        // Move to the next line
         printf("\n");
     }
+}
 
+int main() {
+    int N;
+    printf("Enter the number of rows: ");
+    scanf("%d", &N);
+    
+    print_palindromic_triangle(N);
+    
     return 0;
 }
