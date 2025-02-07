4a77a186ee940ef788aa94dab004e6b296b41c84
AdityaSohal <adityasohal732@gmail.com>
2025-02-07 18:59:27 +0530

    Solution to Prime Number Checker

--- Prime Number Checker@@67a4d240c8f64283936a1643/code.c
+++ Prime Number Checker@@67a4d240c8f64283936a1643/code.c
@@ -1,26 +1,24 @@
 #include <stdio.h>
-#include <stdbool.h>
-int isPrime(int num){
-    if(num<2){
-        isPrime =0;
+
+// Function to check if a number is prime
+int isPrime(int num) {
+    if (num < 2) return 0;               // Numbers less than 2 are not prime
+    if (num == 2) return 1;              // 2 is prime (special case)
+    if (num % 2 == 0) return 0;          // Even numbers greater than 2 are not prime
+    
+    for (int i = 3; i * i <= num; i += 2) {  // Check only odd divisors
+        if (num % i == 0) return 0;      // Not prime if divisible by i
     }
-    for(int i=2;i<n;i++){
-        if(num%i==0){
-            return 0;
-        }
-        else{
-            return 1;
-        }
-    }
+    return 1;                           // Prime if no divisors found
 }
 
-int main(){
+int main() {
     int t;
     scanf("%d", &t);
-    while(t--){
+    while (t--) {
         int num;
         scanf("%d", &num);
-        printf("%d\n",isPrime(num));
+        printf("%d\n", isPrime(num));    // Print 1 if prime, 0 if not
     }
     return 0;
 }
