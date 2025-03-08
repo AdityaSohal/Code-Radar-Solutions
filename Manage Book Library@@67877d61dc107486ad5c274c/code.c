e9ee0bde1ed0272300d64d90ceda9ba286ec8162
Aditya Sohal <adityasohal732@gmail.com>
2025-03-08 19:12:24 +0530

    Solution to Manage Book Library

--- Manage Book Library@@67877d61dc107486ad5c274c/code.c
+++ Manage Book Library@@67877d61dc107486ad5c274c/code.c
@@ -1,29 +1,22 @@
 #include <stdio.h>
 
-struct Book {
+struct Book{
     char title[100];
     char author[100];
     float price;
 };
 
-int main() {
+int main(){
     int n;
+    scanf("%d",n);
     float threshold;
-
-    // Input the number of books
-    scanf("%d", &n);
     struct Book books[n];
 
-    // Taking input for books
-    for (int i = 0; i < n; i++) {
+    for(int i =0;i<n;i++){
         scanf("%s %s %f", books[i].title, books[i].author, &books[i].price);
     }
-
-    // Input the price threshold
-    scanf("%f", &threshold);
-
-    // Display books above the price threshold
-    printf("Books above price %.2f:\n", threshold);
+    scanf("%f",&threshold);
+    printf("Books above price %.2f:\n",threshold);
     for (int i = 0; i < n; i++) {
         if (books[i].price > threshold) {
             printf("Title: %s, Author: %s, Price: %.2f\n", books[i].title, books[i].author, books[i].price);
