8713081b89129346d87c2b3f053571da1c4e8c9e
Aditya Sohal <adityasohal732@gmail.com>
2025-03-08 18:39:36 +0530

    Solution to Calculate Area and Perimeter of Rectangles

---
+++ Calculate Area and Perimeter of Rectangles@@67877d61dc107486ad5c274b/code.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+struct Rectangle{
+    float length;
+    float bredth;
+};
+void calculateareaandperimenter(struct Rectangle react[],int n){
+    for(int i=0;i<n;i++){
+        float area = react[i].length*react[i].bredth;
+        float perimeter = 2*(react[i].length+react[i].bredth);
+        printf("Rectangle %d: Area = %.2f, Perimeter = %.2f\n", i+1,area, perimeter)
+    }
+}
+int main(){
+    int n;
+    scnaf("%d",&n);
+    struct Rectangle react[n];
+    for(int i=0;i<n;i++){
+        scnaf("%f %f",&react[i].length, &react[i].bredth);
+    }
+    calculateareaandperimenter(react,n)
+}
