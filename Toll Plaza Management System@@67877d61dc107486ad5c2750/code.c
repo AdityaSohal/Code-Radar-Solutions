a712d610fb673369cd743cc5424a5f0d01fc25dc
Aditya Sohal <adityasohal732@gmail.com>
2025-03-08 20:41:43 +0530

    Solution to Toll Plaza Management System

--- Toll Plaza Management System@@67877d61dc107486ad5c2750/code.c
+++ Toll Plaza Management System@@67877d61dc107486ad5c2750/code.c
@@ -16,11 +16,20 @@ int main() {
     float carToll = 0, truckToll = 0, bikeToll = 0;
     for (int i = 0; i < n; i++) {
         scanf("%s %s %f", vehicles[i].number, vehicles[i].type, &vehicles[i].toll);
+
+        // Convert type to lowercase (if necessary)
+        for (int j = 0; vehicles[i].type[j]; j++) {
+            if (vehicles[i].type[j] >= 'A' && vehicles[i].type[j] <= 'Z') {
+                vehicles[i].type[j] += 32; // Convert uppercase to lowercase
+            }
+        }
+
+        // Compare with lowercase values
         if (strcmp(vehicles[i].type, "car") == 0) {
             carToll += vehicles[i].toll;
-        } else if (strcmp(vehicles[i].type, "Truck") == 0) {
+        } else if (strcmp(vehicles[i].type, "truck") == 0) {
             truckToll += vehicles[i].toll;
-        } else if (strcmp(vehicles[i].type, "Bike") == 0) {
+        } else if (strcmp(vehicles[i].type, "bike") == 0) {
             bikeToll += vehicles[i].toll;
         }
     }
